f389f434861859b89f5e65d523907d928914f709
Oksana Slusarenko <oksana.slu@gmail.com>
2024-12-19 18:37:42 +0200

    Update urls.py

--- xblock_jupyter_viewer/rest/urls.py
+++ xblock_jupyter_viewer/rest/urls.py
@@ -1,14 +1,14 @@
 """
 Defines a URL to return a notebook html page to be used in an iframe
 """
-from django.conf.urls import url
+from django.urls import re_path
 
 from .views import NotebookViewer
 
 app_name = 'xblock_jupyter_viewer'
 
 urlpatterns = [
-    url(
+    re_path(
     r'^render_notebook/$',
         NotebookViewer.as_view(),
         name='jupyter_nb_viewer'
